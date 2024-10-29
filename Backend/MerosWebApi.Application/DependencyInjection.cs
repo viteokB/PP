78dad07d082677830114ca9cad7fd330342a8ec6
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-10-29 21:28:26 +0500

    Добавление метода конифигурации AppSettings

--- Backend/MerosWebApi.Application/DependencyInjection.cs
+++ Backend/MerosWebApi.Application/DependencyInjection.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MerosWebApi.Application.Common.EmailSender;
 using MerosWebApi.Application.Interfaces;
+using MerosWebApi.Application.Common;
 
 namespace MerosWebApi.Application
 {
@@ -22,18 +23,57 @@ namespace MerosWebApi.Application
             var emailAddress = devConfiguration["UserName"];
             var emailPassword = devConfiguration["Password"];
 
-            if(string.IsNullOrWhiteSpace(hostAddress) || hostPort == default(int) ||
+            if (string.IsNullOrWhiteSpace(hostAddress) || hostPort == default(int) ||
                 string.IsNullOrWhiteSpace(emailAddress) || string.IsNullOrWhiteSpace(emailPassword))
             {
                 throw new ArgumentException($"Не правильно сконфигурирована" +
                     $"секция '{nameof(devConfiguration.Value)}' в 'appsettings.json'");
             }
-            
+
 
             var emailConfiguration = new DevelopmentConfiguration(emailAddress, emailPassword, hostAddress,
                 hostPort);
 
             services.AddSingleton<DevelopmentConfiguration>(emailConfiguration);
+
+            return services;
+        }
+
+        public static IServiceCollection AddAppSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var appSettingsSection = configuration.GetSection("AppSettings");
+
+            var secret = appSettingsSection["Secret"];
+            var name = appSettingsSection["Name"];
+            int.TryParse(appSettingsSection["MaxLoginFailedCount"], out var maxLoginFailed);
+            int.TryParse(appSettingsSection["LoginFailedWaitingTime"], out var loginFailedWait);
+            int.TryParse(appSettingsSection["MaxUnconfirmedEmailCount"], out var maxUnconfEmail);
+            int.TryParse(appSettingsSection["UnconfirmedEmailWaitingTime"], out var unconfEmailWait);
+            var confEmailUrl = appSettingsSection["ConfirmEmailUrl"];
+            int.TryParse(appSettingsSection["MaxResetPasswordCount"], out var maxResetPasswd);
+            int.TryParse(appSettingsSection["ResetPasswordWaitingTime"], out var resetPasswdWait);
+            int.TryParse(appSettingsSection["ResetPasswordValidTime"], out var resetPasswdValid);
+            var resetPasswdUrl = appSettingsSection["ResetPasswordUrl"];
+
+            //В будущем добавить логику проверки полученных значений
+            //И/или переделать конфигурирование AppSettings
+
+            var appSettings = new AppSettings
+            {
+                Secret = secret,
+                Name = name,
+                MaxLoginFailedCount = maxLoginFailed,
+                LoginFailedWaitingTime = loginFailedWait,
+                MaxUnconfirmedEmailCount = maxUnconfEmail,
+                UnconfirmedEmailWaitingTime = unconfEmailWait,
+                ConfirmEmailUrl = confEmailUrl,
+                MaxResetPasswordCount = maxResetPasswd,
+                ResetPasswordWaitingTime = resetPasswdWait,
+                ResetPasswordValidTime = resetPasswdValid,
+                ResetPasswordUrl = resetPasswdUrl
+            };
+
+            services.AddSingleton<AppSettings>(appSettings);
 
             return services;
         }
