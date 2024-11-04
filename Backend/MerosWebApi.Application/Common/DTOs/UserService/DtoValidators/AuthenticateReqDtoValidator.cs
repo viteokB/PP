1c3cd53997c2b38649073b8ed1a60d6fcdf73427
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-11-05 01:42:51 +0500

    Реализовал валидаторы для DTO запросов

---
+++ Backend/MerosWebApi.Application/Common/DTOs/UserService/DtoValidators/AuthenticateReqDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Runtime.Internal.Auth;
+using FluentValidation;
+
+namespace MerosWebApi.Application.Common.DTOs.UserService.DtoValidators
+{
+    public class AuthenticateReqDtoValidator : AbstractValidator<AuthenticateReqDto>
+    {
+        public AuthenticateReqDtoValidator()
+        {
+            RuleFor(a => a.Email).Email();
+        }
+    }
+}
