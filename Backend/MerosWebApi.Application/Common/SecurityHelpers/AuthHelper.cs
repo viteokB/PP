1900f0ff8bba729e89c253e093c75e88c1d241fe
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-11-05 22:25:21 +0500

    Добавил AuthHelper

---
+++ Backend/MerosWebApi.Application/Common/SecurityHelpers/AuthHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using MerosWebApi.Application.Common.Exceptions;
+using MerosWebApi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerosWebApi.Application.Common.SecurityHelpers
+{
+    public class AuthHelper : IAuthHelper
+    {
+        public Guid GetUserId(ControllerBase controller)
+        {
+            if (controller == null)
+                throw new ArgumentNullException(nameof(controller));
+
+            var identity = controller.HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity == null)
+                throw new UnauthorizedException();
+
+            var nameClaim = identity.FindFirst(ClaimTypes.Name);
+            if (nameClaim == null) throw new ApplicationException("Cannot get claim 'Name'.");
+
+            return Guid.Parse(nameClaim.Value);
+        }
+    }
+}
---
+++ Backend/MerosWebApi.Application/Interfaces/IAuthHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerosWebApi.Application.Interfaces
+{
+    public interface IAuthHelper
+    {
+        Guid GetUserId(ControllerBase controller);
+    }
+}
